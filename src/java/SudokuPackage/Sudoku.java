cdef8d61046eab9852150396fb584c60232a6920
SteveBindels <steve_bindels@hotmail.com>
2017-03-28 10:22:40 +0200

    maak grid

---
+++ src/java/SudokuPackage/Sudoku.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SudokuPackage;
+
+/**
+ *
+ * @author steve
+ */
+public class Sudoku {
+    static int grid[][]=new int[9][9];
+}
